4b3213f0304d0afd75bf383523b5890d41a763e5
Gian Carlo Gilos <gianCarlo.gilos@outlook.com>
2018-09-06 16:24:18 +0800

    Removed jest env since it clouded linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
+const HtmlWebpackPlugin= require ('html-webpack-plugin')
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: './src/index.js',
   output: {
