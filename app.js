012e6490aedf6944502128f0e209b7cf8c1adfe2
Gian Carlo Gilos <gianCarlo.gilos@outlook.com>
2018-09-08 17:04:27 +0800

    Now using express to server up application instead of webpack-dev-server

---
+++ app.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import express from 'express'
+import webpack from 'webpack'
+import config from './webpack.dev'
+import open from 'open'
+
+const port = 3000
+const app = express()
+const compiler = webpack(config)
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+
+app.use(require('webpack-hot-middleware')(compiler))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './src/index.html'))
+})
+
+app.listen(port, (err) => {
+  if (err) { console.log(err) } else { open(`http:localhost:${port}`) }
+})
