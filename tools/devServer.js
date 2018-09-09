9fe8cff09e769a84bc26c6bfe69a984e0012459c
Gian Carlo Gilos <gianCarlo.gilos@outlook.com>
2018-09-09 10:27:57 +0800

    Pointed start to proper directory of devServer and also fixing directory directives of dependencies

--- tools/devServer.js
+++ tools/devServer.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import express from 'express'
 import webpack from 'webpack'
-import config from './webpack.dev'
+import config from '../webpack.dev'
 import open from 'open'
 
 const port = 3000
@@ -16,7 +16,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './src/index.html'))
+  res.sendFile(path.join(__dirname, '../src/index.html'))
 })
 
 app.listen(port, (err) => {
