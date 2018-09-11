ba286b7fa8d25bbdf612a7a4f20a622c15435636
Gian Carlo Gilos <gianCarlo.gilos@outlook.com>
2018-09-12 07:46:51 +0800

    Fixed naming of constructors from camelCase to PascalCase

--- tools/devServer.js
+++ tools/devServer.js
@@ -3,19 +3,19 @@ import express from 'express'
 import webpack from 'webpack'
 import config from '../webpack.dev'
 import open from 'open'
-import hotMiddleware from 'webpack-hot-middleware'
-import devMiddleware from 'webpack-dev-middleware'
+import HotReloadMiddleware from 'webpack-hot-middleware'
+import DevMiddleware from 'webpack-dev-middleware'
 
 const port = 3000
 const app = express()
 const compiler = webpack(config)
 
-app.use(new devMiddleware(compiler, {
+app.use(new DevMiddleware(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }))
 
-app.use(new hotMiddleware(compiler))
+app.use(new HotReloadMiddleware(compiler))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'))
