9b43d03e8fd83a6e42d3e31319f5a40996ad5eba
Gian Carlo Gilos <gianCarlo.gilos@outlook.com>
2018-09-13 08:54:18 +0800

    Fixed static content directory: from src to dist

--- tools/devServer.js
+++ tools/devServer.js
@@ -18,7 +18,7 @@ app.use(new DevMiddleware(compiler, {
 app.use(new HotReloadMiddleware(compiler))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.html'))
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
 app.listen(port, (err) => {
